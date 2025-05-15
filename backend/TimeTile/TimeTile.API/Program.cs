a2d9d1f3db3b027e9c16d30e9a23be21c7311f93
Nikita Developer <nik0609tak@gmail.com>
2025-05-15 23:48:19 +0200

    Serilog is added.

--- backend/TimeTile/TimeTile.API/Program.cs
+++ backend/TimeTile/TimeTile.API/Program.cs
@@ -1,25 +1,38 @@
-using Microsoft.EntityFrameworkCore;
+using Serilog;
 using TimeTile.API;
-using TimeTile.Storage.Contexts;
 
-// Log.Logger = new LoggerConfiguration()
-//     .WriteTo.Console()
-//     .CreateBootstrapLogger();
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
 try
 {
-    //Log.Information("Starting web application");
+    Log.Information("Starting web application");
+    
     var builder = WebApplication.CreateBuilder(args);
+    
+    DotNetEnv.Env.TraversePath().Load();
+    
+    builder.Host.UseSerilog((context, services, configuration) =>
+    {
+        configuration
+            .ReadFrom.Configuration(context.Configuration)
+            .ReadFrom.Services(services)
+            .Enrich.FromLogContext()
+            .WriteTo.Console();
+    });
+    
     builder.AddServices();
+    
     var app = builder.Build();
     await app.Configure();
     app.Run();
 }
 catch (Exception ex)
 {
-    //Log.Fatal(ex, "Application terminated unexpectedly");
+    Log.Fatal(ex, "Application terminated unexpectedly");
 }
 finally
 {
-    //Log.CloseAndFlush();
+    Log.CloseAndFlush();
 }

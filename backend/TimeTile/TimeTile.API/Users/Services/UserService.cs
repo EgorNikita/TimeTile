82c8944a47c4087a4707534c715711602b448879
Nikita Developer <nik0609tak@gmail.com>
2025-05-23 22:06:19 +0200

    User Services was added

---
+++ backend/TimeTile/TimeTile.API/Users/Services/UserService.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+using System.Security.Cryptography;
+
+namespace TimeTile.API.Users.Services;
+
+public class UserService : IUserService
+{
+    public Task<string> GenerateLogin(string firstname, string lastname, int birthYear, string institutionDomain)
+    {
+        var fn = firstname.Length >= 4 
+            ? firstname[..4].ToLower(CultureInfo.InvariantCulture) 
+            : firstname.ToLower(CultureInfo.InvariantCulture);
+
+        var ln = lastname.Length >= 5 
+            ? lastname[..5].ToLower(CultureInfo.InvariantCulture) 
+            : lastname.ToLower(CultureInfo.InvariantCulture);
+
+        var login = $"{fn}.{ln}{birthYear}@{institutionDomain}";
+        return Task.FromResult(login);
+    }
+
+    public Task<string> GenerateDefaultPassword(string firstname, string lastname, short birthYear)
+    {
+        var basePart = $"{firstname[..1].ToUpper(CultureInfo.InvariantCulture)}{lastname[..1].ToLower(CultureInfo.InvariantCulture)}{birthYear % 100:D2}";
+        var suffix = RandomNumberGenerator.GetInt32(1000, 10000);
+        var password = $"{basePart}{suffix}";
+        return Task.FromResult(password);
+    }
+
+    public Task<string> GenerateDefaultAvatar(string firstname, string lastname)
+    {
+        var initials = $"{char.ToUpper(firstname[0], CultureInfo.InvariantCulture)}{char.ToUpper(lastname[0], CultureInfo.InvariantCulture)}";
+        return Task.FromResult($"/avatars/{initials}.png");
+    }
+}

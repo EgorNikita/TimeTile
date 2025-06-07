ec70ac002a9a40500680ec53e923a7a98bc3c938
loxx3450 <egorka231231@gmail.com>
2025-06-07 16:49:59 +0200

    PagedList is added

---
+++ backend/TimeTile/TimeTile.API/Common/Api/Extensions/PaginationDatabaseExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TimeTile.API.Common.Api.Pagination.PagedRequest;
+using TimeTile.API.Common.Api.Pagination;
+
+namespace TimeTile.API.Common.Api.Extensions
+{
+    public static class PaginationDatabaseExtensions
+    {
+        private const int DefaultPage = 1;
+        private const int DefaultPageSize = 10;
+
+        public static async Task<PagedList<TResponse>> ToPagedListAsync<TRequest, TResponse>(
+            this IQueryable<TResponse> query, 
+            TRequest request, 
+            CancellationToken cancellationToken = default
+        ) where TRequest : IPagedRequest
+        {
+            var page = request.Page ?? DefaultPage;
+            var pageSize = request.PageSize ?? DefaultPageSize;
+
+            ArgumentOutOfRangeException.ThrowIfLessThanOrEqual(page, 0);
+            ArgumentOutOfRangeException.ThrowIfLessThanOrEqual(pageSize, 0);
+            ArgumentOutOfRangeException.ThrowIfGreaterThan(pageSize, IPagedRequest.MaxPageSize);
+
+            var totalItems = await query.CountAsync(cancellationToken);
+
+            var totalPages = (totalItems / pageSize) + 1;
+
+            var items = await query
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync(cancellationToken);
+
+            return new PagedList<TResponse>(items, page, pageSize, totalPages, totalItems);
+        }
+    }
+}

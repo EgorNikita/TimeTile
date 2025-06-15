994b719b36516ebad5a7a7f92afd9994ec8be096
loxx3450 <egorka231231@gmail.com>
2025-06-15 21:06:16 +0200

    Addition of RequireInstitutionFilter

---
+++ backend/TimeTile/TimeTile.API/Common/Api/Extensions/HttpContextExtensions.cs
@@ -0,0 +1,12 @@
+﻿using TimeTile.API.Common.Constants;
+
+namespace TimeTile.API.Common.Api.Extensions
+{
+    public static class HttpContextExtensions
+    {
+        public static int GetInstitutionId(this HttpContext context)
+        {
+            return (int) context.Items[HttpContextItemKeys.InstitutionId]!;
+        }
+    }
+}
---
+++ backend/TimeTile/TimeTile.API/Common/Api/Extensions/RouteGroupBuilderExtensions.cs
@@ -0,0 +1,12 @@
+﻿using TimeTile.API.Common.Api.Filters;
+
+namespace TimeTile.API.Common.Api.Extensions
+{
+    public static class RouteGroupBuilderExtensions
+    {
+        public static RouteGroupBuilder RequireInstitution(this RouteGroupBuilder group)
+        {
+            return group.AddEndpointFilter<RequireInstitutionFilter>();
+        }
+    }
+}

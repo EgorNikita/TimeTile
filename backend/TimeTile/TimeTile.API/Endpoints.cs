e213a7f00e4353262c6c3e1fcd68d6ba9b6e6f5f
loxx3450 <egorka231231@gmail.com>
2025-06-17 17:11:43 +0200

    CreateTimetableUnitEndpoint is added

--- backend/TimeTile/TimeTile.API/Endpoints.cs
+++ backend/TimeTile/TimeTile.API/Endpoints.cs
@@ -12,6 +12,7 @@ using TimeTile.API.Roles.Endpoints.CreateRole;
 using TimeTile.API.Students.Endpoints.CreateStudent;
 using TimeTile.API.Students.Endpoints.GetStudentById;
 using TimeTile.API.Students.Endpoints.GetStudents;
+using TimeTile.API.TimetableUnits.Endpoints.Create;
 using TimeTile.API.TimetableUnits.Endpoints.Get;
 using TimeTile.API.TimetableUnits.Endpoints.GetById;
 using TimeTile.Core.Common.Constants;
@@ -62,6 +63,8 @@ public static class Endpoints
         endpoints.MapEndpoint<GetTimetableUnitsEndpoint>();
 
         endpoints.MapEndpoint<GetTimetableUnitByIdEndpoint>();
+
+        endpoints.MapEndpoint<CreateTimetableUnitEndpoint>();
     }
 
     private static void MapStudentEndpoints(this IEndpointRouteBuilder app)
---
+++ backend/TimeTile/TimeTile.API/TimetableUnits/Endpoints/Create/RequestValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using TimeTile.API.Common;
+using TimeTile.API.Common.Api.Extensions;
+using TimeTile.Core.Common.Regex;
+
+namespace TimeTile.API.TimetableUnits.Endpoints.Create
+{
+    public class RequestValidator : AbstractValidator<CreateTimetableUnitEndpoint.Request>
+    {
+        public RequestValidator()
+        {
+            RuleFor(x => x.Title)
+                .Must(x => string.IsNullOrEmpty(x) || InputSanitizer.Sanitize(x) == x)
+                .WithMessage("Title contains invalid characters.")
+                .ApplyRegexPattern(RegexPatterns.Pattern.Title);
+
+            RuleFor(x => x.EndTime)
+                .GreaterThan(x => x.StartTime)
+                .WithMessage("EndTime cannot be before StartTime.");
+        }
+    }
+}

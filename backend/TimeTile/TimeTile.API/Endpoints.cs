85b74cf071dd95316e45f062c7fc719ca03b1acf
loxx3450 <egorka231231@gmail.com>
2025-06-17 17:11:21 +0200

    GetTimetableUnitByIdEndpoint is added

--- backend/TimeTile/TimeTile.API/Endpoints.cs
+++ backend/TimeTile/TimeTile.API/Endpoints.cs
@@ -13,6 +13,7 @@ using TimeTile.API.Students.Endpoints.CreateStudent;
 using TimeTile.API.Students.Endpoints.GetStudentById;
 using TimeTile.API.Students.Endpoints.GetStudents;
 using TimeTile.API.TimetableUnits.Endpoints.Get;
+using TimeTile.API.TimetableUnits.Endpoints.GetById;
 using TimeTile.Core.Common.Constants;
 
 namespace TimeTile.API;
@@ -59,6 +60,8 @@ public static class Endpoints
             .RequireInstitution();
 
         endpoints.MapEndpoint<GetTimetableUnitsEndpoint>();
+
+        endpoints.MapEndpoint<GetTimetableUnitByIdEndpoint>();
     }
 
     private static void MapStudentEndpoints(this IEndpointRouteBuilder app)

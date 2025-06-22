8934779c9546e43473627301d6e7e4aed2166d69
loxx3450 <egorka231231@gmail.com>
2025-06-22 23:16:42 +0200

    GetUserPermissionsEndpoint is added

--- backend/TimeTile/TimeTile.API/Endpoints.cs
+++ backend/TimeTile/TimeTile.API/Endpoints.cs
@@ -26,6 +26,7 @@ using TimeTile.API.TimetableUnits.Endpoints.Create;
 using TimeTile.API.TimetableUnits.Endpoints.Get;
 using TimeTile.API.TimetableUnits.Endpoints.GetById;
 using TimeTile.Core.Common.Constants;
+using TimeTile.API.Users.Endpoints.GetPermissions;
 
 namespace TimeTile.API;
 
@@ -40,6 +41,7 @@ public static class Endpoints
         app.MapTimetableUnitsEndpoints();
         app.MapClassroomsEndpoints();
         app.MapStudentEndpoints();
+        app.MapUsersEndpoints();
         app.MapRolesEndpoints();
         app.MapInstitutionEndpoints();
         app.MapFilesEndpoints();
@@ -134,6 +136,15 @@ public static class Endpoints
 
         endpoints.MapEndpoint<GetStudentsEndpoint>()
             .RequireAuthorization(Permissions.Students.Get);
+    }
+
+    private static void MapUsersEndpoints(this IEndpointRouteBuilder app)
+    {
+        var endpoints = app.MapGroup("/users")
+            .WithTags("Users")
+            .RequireInstitution();
+
+        endpoints.MapEndpoint<GetUserPermissionsEndpoint>();
     }
 
     private static void MapRolesEndpoints(this IEndpointRouteBuilder app)

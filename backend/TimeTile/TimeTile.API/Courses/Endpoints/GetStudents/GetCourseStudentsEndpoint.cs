9999d2d6b9ff56483ed799ddea0c0182a7121c30
loxx3450 <egorka231231@gmail.com>
2025-06-29 00:10:11 +0200

    GetCourseStudentsEndpoint is added

---
+++ backend/TimeTile/TimeTile.API/Courses/Endpoints/GetStudents/GetCourseStudentsEndpoint.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata;
+using TimeTile.API.Common.Api;
+using TimeTile.API.Common.Api.Extensions;
+using TimeTile.API.Common.Api.Http;
+using TimeTile.API.Common.Api.Pagination;
+using TimeTile.API.Common.Api.Pagination.PagedRequest;
+using TimeTile.API.Common.Api.Requests;
+using TimeTile.Core.Common.UnifiedResponse;
+using TimeTile.Storage.Contexts;
+
+namespace TimeTile.API.Courses.Endpoints.GetStudents
+{
+    public class GetCourseStudentsEndpoint : IEndpoint
+    {
+        public static IEndpointConventionBuilder Map(IEndpointRouteBuilder app)
+        {
+            return app
+                .MapGet("/{Id:int}/students", Handle)
+                .WithSummary("Gets Students of the Course")
+                .WithRequestValidation<Request>();
+        }
+
+        private static async Task<Ok<Result<PagedList<Response>>>> Handle(
+            [AsParameters] Request request,
+            TimetileDbContext db,
+            CancellationToken cancellationToken)
+        {
+            var relations = await db.CoursesStudents
+                .AsNoTracking()
+                .Where(cs => cs.CourseId == request.Id)
+                .ApplySorting(
+                    request.SortBy,
+                    request.Descending
+                )
+                .Select(cs => new Response(
+                    cs.StudentId,
+                    new StudentInfo(
+                        cs.Student.Id,
+                        cs.Student.Firstname,
+                        cs.Student.Lastname,
+                        cs.Student.HomeAddress,
+                        cs.Student.PhoneNumber,
+                        cs.Student.BirthDate,
+                        cs.Student.Login,
+                        cs.Student.GroupId,
+                        cs.Student.Avatar.FileGuid.ToString()
+                    ),
+                    cs.ExamGradeId,
+                    cs.HasExam,
+                    cs.PositionX,
+                    cs.PositionY
+                ))
+                .ToPagedListAsync(request, cancellationToken);
+
+            var result = Result.Success(relations);
+
+            return TypedResults.Ok(result);
+        }
+
+        public sealed record Request(
+            int Id,
+            int? Page = 1,
+            int? PageSize = 10,
+            string? SortBy = null,
+            bool Descending = false
+        ) : IPagedRequest, ISortRequest;
+
+        private sealed record Response(
+            int StudentId,
+            StudentInfo Student,
+            int? ExamGradeId,
+            bool HasExam,
+            short PositionX,
+            short PositionY
+        );
+
+        private sealed record StudentInfo(
+            int Id,
+            string Firstname,
+            string Lastname,
+            string HomeAddress,
+            string PhoneNumber,
+            DateOnly BirthDate,
+            string Login,
+            int? GroupId,
+            string AvatarUrl
+        );
+    }
+}
--- backend/TimeTile/TimeTile.API/Endpoints.cs
+++ backend/TimeTile/TimeTile.API/Endpoints.cs
@@ -50,6 +50,7 @@ using TimeTile.API.Courses.Endpoints.Get;
 using TimeTile.API.Courses.Endpoints.GetById;
 using TimeTile.API.Courses.Endpoints.Create;
 using TimeTile.API.Courses.Endpoints.UpdateStudents;
+using TimeTile.API.Courses.Endpoints.GetStudents;
 
 namespace TimeTile.API;
 
@@ -297,7 +298,8 @@ public static class Endpoints
         endpoints.MapEndpoint<GetCoursesEndpoint>()
             .MapEndpoint<GetCourseByIdEndpoint>()
             .MapEndpoint<CreateCourseEndpoint>()
-            .MapEndpoint<UpdateCourseStudentsEndpoint>();
+            .MapEndpoint<UpdateCourseStudentsEndpoint>()
+            .MapEndpoint<GetCourseStudentsEndpoint>();
     }
 
 

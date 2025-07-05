b081a85d44c39ffc5d3a6d8fa98dd30ca94babbc
loxx3450 <egorka231231@gmail.com>
2025-07-05 19:03:42 +0200

    Course can have Icon

--- backend/TimeTile/TimeTile.Core/Models/File.cs
+++ backend/TimeTile/TimeTile.Core/Models/File.cs
@@ -24,6 +24,7 @@ namespace TimeTile.Core.Models
 
         public virtual User? User { get; set; }
         public virtual Group? Group { get; set; }
+        public virtual Course Course { get; set; }
         public virtual ClassroomType? ClassroomType { get; set; }
     }
 }

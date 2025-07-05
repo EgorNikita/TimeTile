afc632da7e48359a932282b614011171df3b72a4
loxx3450 <egorka231231@gmail.com>
2025-07-05 17:39:42 +0200

    UpdateGroupInstitutionMembersEndpoint is added

--- backend/TimeTile/TimeTile.API/Endpoints.cs
+++ backend/TimeTile/TimeTile.API/Endpoints.cs
@@ -56,6 +56,7 @@ using TimeTile.API.Courses.Endpoints.GetById;
 using TimeTile.API.Courses.Endpoints.Create;
 using TimeTile.API.Courses.Endpoints.UpdateStudents;
 using TimeTile.API.Groups.Endpoints.UpdateStudents;
+using TimeTile.API.Groups.Endpoints.UpdateInstitutionMembers;
 
 namespace TimeTile.API;
 
@@ -297,7 +298,8 @@ public static class Endpoints
         endpoints.MapEndpoint<GetGroupsEndpoint>()
             .MapEndpoint<GetGroupByIdEndpoint>()
             .MapEndpoint<CreateGroupEndpoint>()
-            .MapEndpoint<UpdateGroupStudentsEndpoint>();
+            .MapEndpoint<UpdateGroupStudentsEndpoint>()
+            .MapEndpoint<UpdateGroupInstitutionMembersEndpoint>();
     }
 
     private static void MapLessonsEndpoints(this IEndpointRouteBuilder app)

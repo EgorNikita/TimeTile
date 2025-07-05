f9edb8e89e66f51e2a2abeee1e108ee8a6d6bb8b
loxx3450 <egorka231231@gmail.com>
2025-07-05 19:52:39 +0200

    File's Navigation Property "Course" is nullable

--- backend/TimeTile/TimeTile.Core/Models/File.cs
+++ backend/TimeTile/TimeTile.Core/Models/File.cs
@@ -23,7 +23,7 @@ namespace TimeTile.Core.Models
         public Guid FileGuid { get; set; }
 
         public virtual User? User { get; set; }
-        public virtual Course Course { get; set; }
+        public virtual Course? Course { get; set; }
         public virtual ClassroomType? ClassroomType { get; set; }
     }
 }

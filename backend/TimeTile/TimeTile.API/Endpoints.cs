a56421b9068fe3cf67bb84ef17ff24f1512501b4
loxx3450 <egorka231231@gmail.com>
2025-07-06 18:47:40 +0200

    GetSubjectsBulkEndpoint is added

--- backend/TimeTile/TimeTile.API/Endpoints.cs
+++ backend/TimeTile/TimeTile.API/Endpoints.cs
@@ -60,6 +60,7 @@ using TimeTile.API.Students.Endpoints.GetCourses;
 using TimeTile.API.Courses.Endpoints.UpdateCourseToStudent;
 using TimeTile.API.Groups.Endpoints.UpdateStudents;
 using TimeTile.API.Groups.Endpoints.UpdateInstitutionMembers;
+using TimeTile.API.Subjects.Endpoints.GetBulk;
 
 namespace TimeTile.API;
 
@@ -294,7 +295,8 @@ public static class Endpoints
 
         endpoints.MapEndpoint<GetSubjectsEndpoint>()
             .MapEndpoint<GetSubjectByIdEndpoint>()
-            .MapEndpoint<CreateSubjectEndpoint>();
+            .MapEndpoint<CreateSubjectEndpoint>()
+            .MapEndpoint<GetSubjectsBulkEndpoint>();
     }
 
     private static void MapGroupsEndpoints(this IEndpointRouteBuilder app)
---
+++ backend/TimeTile/TimeTile.API/Subjects/Endpoints/GetBulk/GetSubjectsBulkEndpoint.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using TimeTile.API.Common.Api;
+using TimeTile.API.Common.Api.Extensions;
+using TimeTile.API.Common.Api.Http;
+using TimeTile.API.Common.Api.Pagination;
+using TimeTile.API.Common.Api.Pagination.PagedRequest;
+using TimeTile.API.Common.Api.Requests;
+using TimeTile.Core.Common.UnifiedResponse;
+using TimeTile.Storage.Contexts;
+
+namespace TimeTile.API.Subjects.Endpoints.GetBulk
+{
+    public class GetSubjectsBulkEndpoint : IEndpoint
+    {
+        public static IEndpointConventionBuilder Map(IEndpointRouteBuilder app)
+        {
+            return app
+                .MapGet("/bulk", Handle)
+                .WithSummary("Returns subjects by passed ids")
+                .WithRequestValidation<Request>();
+        }
+
+        private static async Task<Ok<Result<List<Response>>>> Handle(
+            [AsParameters] Request request,
+            TimetileDbContext db,
+            IInstitutionProvider institutionProvider,
+            CancellationToken cancellationToken)
+        {
+            var institutionId = institutionProvider.GetInstitutionId();
+
+            // Form a final paged list
+            var subjects = await db.Subjects
+                .AsNoTracking()
+                .Where(s => request.Ids.Contains(s.Id))
+                .Select(x => new Response
+                (
+                    x.Id,
+                    x.Title
+                ))
+                .ToListAsync(cancellationToken);
+
+            var result = Result.Success(subjects);
+
+            return TypedResults.Ok(result);
+        }
+
+        public sealed record Request(
+            int[] Ids
+        );
+
+        private sealed record Response(
+            int Id,
+            string Title
+        );
+    }
+}

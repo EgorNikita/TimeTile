5396ac2a58a3702b831149b49959a1f56c2597ae
loxx3450 <egorka231231@gmail.com>
2025-07-09 22:19:52 +0200

    GetAssignmentByIdEndpoint is added

---
+++ backend/TimeTile/TimeTile.API/Assignments/Endpoints/GetById/GetAssignmentByIdEndpoint.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using TimeTile.API.Common.Api;
+using TimeTile.API.Common.Api.Extensions;
+using TimeTile.API.Common.Api.Pagination.PagedRequest;
+using TimeTile.API.Common.Api.Requests;
+using TimeTile.Core.Common.UnifiedResponse;
+using TimeTile.Storage.Contexts;
+
+namespace TimeTile.API.Assignments.Endpoints.GetById
+{
+    public class GetAssignmentByIdEndpoint : IEndpoint
+    {
+        public static IEndpointConventionBuilder Map(IEndpointRouteBuilder app)
+        {
+            return app
+                .MapGet("/{id}", Handle)
+                .WithSummary("Returns Assignment by passed Id")
+                .WithRequestValidation<Request>();
+        }
+
+        private static async Task<Ok<Result<Response>>> Handle(
+            [AsParameters] Request request,
+            TimetileDbContext db,
+            CancellationToken cancellationToken)
+        {
+            // Find Assignment
+            var assignment = await db.Assignments
+                .AsNoTracking()
+                .Include(a => a.AssignmentToFiles)
+                .FirstAsync(x => x.Id == request.Id, cancellationToken);
+
+            var response = new Response(
+                assignment.Id,
+                assignment.Title,
+                assignment.Description,
+                assignment.PublishedAt,
+                assignment.Deadline,
+                assignment.UploadAfterDeadline,
+                assignment.AssignmentToFiles.Any()
+            );
+
+            var result = Result.Success(response);
+
+            return TypedResults.Ok(result);
+        }
+
+        public sealed record Request(
+            int Id
+        );
+
+        private sealed record Response(
+            int Id,
+            string Title,
+            string Description,
+            DateTimeOffset PublishedAt,
+            DateTimeOffset Deadline,
+            bool UploadAfterDeadline,
+            bool HasAttachments
+        );
+    }
+}
--- backend/TimeTile/TimeTile.API/Endpoints.cs
+++ backend/TimeTile/TimeTile.API/Endpoints.cs
@@ -62,6 +62,7 @@ using TimeTile.API.Groups.Endpoints.UpdateStudents;
 using TimeTile.API.Groups.Endpoints.UpdateInstitutionMembers;
 using TimeTile.API.Subjects.Endpoints.GetBulk;
 using TimeTile.API.Assignments.Endpoints.Get;
+using TimeTile.API.Assignments.Endpoints.GetById;
 
 namespace TimeTile.API;
 
@@ -342,7 +343,8 @@ public static class Endpoints
     {
         var endpoints = app.CreateInstitutionGroup(Routes.Assignments, Tags.Assignments);
 
-        endpoints.MapEndpoint<GetAssignmentsEndpoint>();
+        endpoints.MapEndpoint<GetAssignmentsEndpoint>()
+            .MapEndpoint<GetAssignmentByIdEndpoint>();
     }
 
 

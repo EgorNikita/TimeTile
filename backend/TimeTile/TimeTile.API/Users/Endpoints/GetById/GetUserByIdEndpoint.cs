7777c3e4ad1bfcc1968e670cd9f3cabdc27d560c
Kliushyn Yehor <egorka231231@gmail.com>
2025-07-26 11:06:59 +0200

    GetUserByIdEndpoint is added

---
+++ backend/TimeTile/TimeTile.API/Users/Endpoints/GetById/GetUserByIdEndpoint.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using TimeTile.API.Common.Api;
+using TimeTile.API.Common.Api.Extensions;
+using TimeTile.Core.Common.Interfaces.Services;
+using TimeTile.Core.Common.UnifiedResponse;
+using TimeTile.Storage.Contexts;
+
+namespace TimeTile.API.Users.Endpoints.GetById
+{
+    public class GetUserByIdEndpoint : IEndpoint
+    {
+        public static IEndpointConventionBuilder Map(IEndpointRouteBuilder app)
+        {
+            return app
+                .MapGet("/{id}", Handle)
+                .WithSummary("Returns User by passed Id")
+                .WithRequestValidation<Request>();
+        }
+
+        private static async Task<Ok<Result<Response>>> Handle(
+            [AsParameters] Request request,
+            TimetileDbContext db,
+            CancellationToken cancellationToken)
+        {
+            // Find User
+            var user = await db.Users
+                .AsNoTracking()
+                .Include(u => u.Avatar)
+                .FirstAsync(u => u.Id == request.Id, cancellationToken);
+
+            var response = new Response(
+                user.Id,
+                user.Firstname,
+                user.Lastname,
+                user.HomeAddress,
+                user.PhoneNumber,
+                user.BirthDate,
+                user.Login,
+                user.Avatar.FileGuid.ToString()
+            );
+
+            var result = Result.Success(response);
+
+            return TypedResults.Ok(result);
+        }
+
+        public sealed record Request(
+            int Id
+        );
+
+        public sealed record Response(
+            int Id,
+            string Firstname,
+            string Lastname,
+            string HomeAddress,
+            string PhoneNumber,
+            DateOnly BirthDate,
+            string Login,
+            string AvatarUrl
+        );
+    }
+}
